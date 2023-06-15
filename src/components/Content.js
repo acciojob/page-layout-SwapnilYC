433ce70b60de28f404126429f265d841639b9af6
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-15 09:05:48 +0000

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Content({content}){
+return <h3>{content}</h3>
+}
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header({header}){
+return <h1>{header}</h1>
+}
+
+export default Header;
